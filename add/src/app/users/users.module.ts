ea4cfb3190efe408a3cbce4737a4e5936eabd6ff
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-07-18 12:09:31 +0100

    add

---
+++ add/src/app/users/users.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListUsersComponent } from './list-users/list-users.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListUsersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsersModule { }
