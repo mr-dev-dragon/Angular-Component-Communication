01bcbec9f4e75b5a1d67b7738714f392080b5db6
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-07-28 15:27:46 +0100

    add

---
+++ a/src/app/mychild/components/from/from.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-from',
+  templateUrl: './from.component.html',
+  styleUrls: ['./from.component.css']
+})
+export class FromComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- a/src/app/mychild/mychild.module.ts
+++ a/src/app/mychild/mychild.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChildComponent } from './components/child/child.component';
 import { PerentComponent } from './components/perent/perent.component';
+import { AlertComponent } from './components/alert/alert.component';
+import { FromComponent } from './components/from/from.component';
 
 
 
 @NgModule({
-  declarations: [ChildComponent, PerentComponent],
+  declarations: [ChildComponent, PerentComponent, AlertComponent, FromComponent],
   imports: [CommonModule],
   exports: [ChildComponent, PerentComponent],
   providers: [],
